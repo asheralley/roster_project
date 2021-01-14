8c19ece20b1dce5c191f9b597e7e965245f8c04f
asheralley <asher.asheralley@gmail.com>
2021-01-14 21:00:58 +1300

    added loop for day headings and offset day index to run a week starting monday

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -5,23 +5,42 @@ const getCurrentDate = new Date();
 const currentDate = getCurrentDate.getDate();
 const currentDay = getCurrentDate.getDay();
 const currentMonthIndex = getCurrentDate.getMonth();
+const currentYear = getCurrentDate.getFullYear();
 const months = [
-  "January",
-  "Febuary",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
 const currentMonth = months[currentMonthIndex];
 
+const firstOfMonth = new Date(`August 1, ${currentYear}`);
+const monthStartDayIndex = firstOfMonth.getDay();
+let offSetDateIndex = monthStartDayIndex - 1 < 0 ? 6 : monthStartDayIndex - 1;
+const monthStartDay = days[offSetDateIndex];
+
+console.log(offSetDateIndex);
+// week -> row
+// day that month starts on
+// how many days in month -> how many weeks
 const Calendar = (props) => {
   return (
     <Container>
@@ -29,67 +48,9 @@ const Calendar = (props) => {
         <h1>{currentMonth}</h1>
       </Row>
       <Row>
-        <Col>Monday</Col>
-        <Col>Tuesday</Col>
-        <Col>Wednesday</Col>
-        <Col>Thursday</Col>
-        <Col>Friday</Col>
-        <Col>Saturday</Col>
-        <Col>Sunday</Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
+        {days.map((day) => (
+          <Col>{day}</Col>
+        ))}
       </Row>
     </Container>
   );
