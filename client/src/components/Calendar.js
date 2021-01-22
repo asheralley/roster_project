706aa8c928b90fe16cc1dcd76edaa6447e76eda7
asheralley <asher.asheralley@gmail.com>
2021-01-22 14:29:42 +1300

    abstracted the date functionality to utility functions file

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import { getCurrentMonth, getDateArray } from "../js/utility.js";
 import DayEntry from "./DayEntry";
 
-const daysArrayTest = getDateArray(currentMonthIndex, currentYear);
-const test = splitArray(daysArrayTest);
+// const daysArrayTest = getDateArray(currentMonthIndex, currentYear);
+// const test = splitArray(daysArrayTest);
+// const currentMonth = visableDaysArray.testMonthValue;
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const getcurrentDate = new Date();
+const currentDay = getcurrentDate.getDay();
+const currentMonth = getcurrentDate.getMonth();
+const currentYear = getcurrentDate.getFullYear();
+const visableDaysArray = getDateArray(currentMonth, currentYear);
+const month = getCurrentMonth(currentYear, currentMonth, currentDay);
 
 const Calendar = (props) => {
   return (
     <Container>
       <Row>
-        <h1>{currentMonth}</h1>
+        <h1>{month}</h1>
       </Row>
       <Row>
         {days.map((day) => (
           <Col>{day}</Col>
         ))}
       </Row>
-      {test.map((el) => {
+      {visableDaysArray.map((el) => {
         return (
           <Row>
             {el.map((dayEl) => (
--- client/src/js/utility.js
+++ client/src/js/utility.js
@@ -1,47 +1,35 @@
 // utility funtions for Calendar component
-function daysInMonth(month, year) {
-  const getCurrentDate = new Date();
-  const currentDate = getCurrentDate.getDate();
-  const currentDay = getCurrentDate.getDay();
+export function getCurrentMonth(year, month, day) {
+  const getCurrentDate = new Date(year, month, day);
   const currentMonthIndex = getCurrentDate.getMonth();
   const currentYear = getCurrentDate.getFullYear();
   const months = [
-      "January",
-      "Febuary",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    days = [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ];
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
   const currentMonth = months[currentMonthIndex];
 
-  const firstOfMonth = new Date(`${currentMonth} 1, ${currentYear}`);
-  const monthStartDayIndex = firstOfMonth.getDay();
-  // may need to make offSetDateIndex a function
-  let offSetDateIndex = monthStartDayIndex - 1 < 0 ? 6 : monthStartDayIndex - 1;
-  const monthStartDay = days[offSetDateIndex];
+  // const firstOfMonth = new Date(`${currentMonth} 1, ${currentYear}`);
+  // const monthStartDayIndex = firstOfMonth.getDay();
+  // // may need to make offSetDateIndex a function
+  // let offSetDateIndex = monthStartDayIndex - 1 < 0 ? 6 : monthStartDayIndex - 1;
+  // const monthStartDay = days[offSetDateIndex];
 
   // function to determine number of days in a given month
   // use non-zero index for month here
-  return new Date(year, month, 0).getDate();
+  return currentMonth;
 }
-
 // week -> row
 // day that month starts on
 // how many days in month -> how many weeks
@@ -57,8 +45,14 @@ function daysInMonth(month, year) {
 // previous month, day that current month starts on.
 // When i start looping i will need to keep track of the weeks in some way.
 
-function getDateArray(currMonth, currYear) {
+export function getDateArray(currMonth, currYear) {
+  function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+  }
   let dateArray = [];
+  let firstOfMonth = new Date(currYear, currMonth);
+  let monthStartDayIndex = firstOfMonth.getDay();
+  let offSetDateIndex = monthStartDayIndex - 1 < 0 ? 6 : monthStartDayIndex - 1;
   let currentMonthOffset = currMonth + 1 > 12 ? 1 : currMonth + 1;
   let previousMonthIndexOffset =
     currentMonthOffset - 1 < 0 ? 11 : currentMonthOffset - 1;
@@ -89,14 +83,28 @@ function getDateArray(currMonth, currYear) {
     dayNumber++;
   }
 
+  function splitArray(array) {
+    let arrayOfArrays = [];
+    while (array.length > 0) {
+      let arrayElement = array.splice(0, 7);
+      arrayOfArrays.push(arrayElement);
+    }
+    return arrayOfArrays;
+  }
+
+  dateArray = splitArray(dateArray);
   return dateArray;
 }
 
-function splitArray(array) {
-  let arrayOfArrays = [];
-  while (array.length > 0) {
-    let arrayElement = array.splice(0, 7);
-    arrayOfArrays.push(arrayElement);
-  }
-  return arrayOfArrays;
-}
+// let testDay = 23;
+// let testMonth = 4;
+// let testYear = 2020;
+// let x = getCurrentMonth(testYear, testMonth, testDay);
+// let y = getDateArray(testMonth, testYear);
+
+// export const visableDaysArray = {
+//   testArray: y,
+//   testMonthValue: x,
+// };
+
+// export default visableDaysArray;
