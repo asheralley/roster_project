6472de9d3be4f3022cc0111e49444857f599a353
asheralley <asher.asheralley@gmail.com>
2021-01-28 15:43:07 +1300

    shifts now added to days array, conditional rendering on Dayentry component

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -19,6 +19,47 @@ const currentMonth = getcurrentDate.getMonth();
 const currentYear = getcurrentDate.getFullYear();
 const visableDaysArray = getDateArray(currentMonth, currentYear);
 const month = getCurrentMonth(currentYear, currentMonth, currentDay);
+const shiftEntries = {
+  "13": {
+    name: "Asher",
+    timeStart: 1000,
+    timeEnd: 1600,
+  },
+  "111": {
+    name: "Asher",
+    timeStart: 900,
+    timeEnd: 1730,
+  },
+  "121": {
+    name: "Asher",
+    timeStart: 900,
+    timeEnd: 1730,
+  },
+  "11": {
+    name: "Asher",
+    timeStart: 900,
+    timeEnd: 1730,
+  },
+};
+
+// don't know the best way to get the data from "shiftEntries" into the DayEntry
+// component.
+
+const addShiftsToDaysArray = (arr) => {
+  for (let week of arr) {
+    for (let dayEl of week) {
+      let arrayDayId = dayEl.dayId;
+      // arrayDayId.toString();
+      if (shiftEntries[arrayDayId]) {
+        dayEl.name = shiftEntries[arrayDayId].name;
+        dayEl.timeStart = shiftEntries[arrayDayId].timeStart;
+        dayEl.timeEnd = shiftEntries[arrayDayId].timeEnd;
+      }
+    }
+  }
+};
+
+addShiftsToDaysArray(visableDaysArray);
 
 const Calendar = (props) => {
   return (
@@ -34,10 +75,10 @@ const Calendar = (props) => {
       {visableDaysArray.map((el) => {
         return (
           <Row>
-            {el.map(({ dayId, day }) => (
+            {el.map(({ dayId, day, name, timeStart, timeEnd }) => (
               <Col id={dayId}>
                 {day}
-                <DayEntry />
+                <DayEntry name={name} timeStart={timeStart} timeEnd={timeEnd} />
               </Col>
             ))}
           </Row>
--- client/src/components/DayEntry.js
+++ client/src/components/DayEntry.js
@@ -1,32 +1,19 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React from "react";
+// import { v4 as uuidv4 } from "uuid";
 
 const DayEntry = (props) => {
-  let day1 = new Date(2021, 0, 12);
-  let day2 = new Date(2021, 0, 13);
-
-  let [month, day, year] = day1.toLocaleDateString().split("/");
-  day1 = `${day}, ${month}, ${year}`;
-  const date2 = day2.toLocaleDateString();
-
-  const shiftEntries = [
-    {
-      id: uuidv4(),
-      name: "Asher",
-      timeStart: 1000,
-      timeEnd: 1600,
-      date: day1,
-    },
-    {
-      id: uuidv4(),
-      name: "Asher",
-      timeStart: 900,
-      timeEnd: 1730,
-      date: date2,
-    },
-  ];
-
-  return <></>;
+  if (props.timeStart == null) {
+    return <></>;
+  } else {
+    return (
+      <>
+        <p>{props.name}</p>
+        <p>
+          {props.timeStart} - {props.timeEnd}
+        </p>
+      </>
+    );
+  }
 };
 
 export default DayEntry;
